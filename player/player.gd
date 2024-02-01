12050ad41513a27c260cf384dcfa4048dcd426df
Kyle Darian <kyleenarson@gmail.com>
2024-01-31 21:52:56 -0500

    Player

--- player/player.gd
+++ player/player.gd
@@ -1,8 +1,6 @@
 extends CharacterBody2D
 
-
 @export var speed: int = 400
-
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -11,4 +9,16 @@ func handleInput():
 
 func _physics_process(delta):
 	handleInput()
+	handleAnimation()
 	move_and_slide()
+
+# Animation
+@onready var sprite = $AnimatedSprite2D
+
+func handleAnimation():
+	
+	if abs(velocity.x) > 0 || abs(velocity.y) > 0:
+		sprite.play("walk")
+		sprite.flip_h = velocity.x < 0
+	else:
+		sprite.stop()
