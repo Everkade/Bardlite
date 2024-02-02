a78b1da6513c0d5f7fc8b3d68596275fdd6a1196
Kyle Darian <kyleenarson@gmail.com>
2024-02-01 19:19:40 -0500

    State files

---
+++ enemy/EnemyIdle.gd
@@ -0,0 +1,19 @@
+extends State
+class_name EnemyIdle
+
+@export var enemy: CharacterBody2D
+
+var direction: Vector2
+
+func enter():
+	direction = Vector2(0, 0)
+	
+func exit():
+	pass
+	
+func update(_delta: float):
+	pass
+	
+func physicsUpdate(_delta: float):
+	if enemy:
+		enemy.velocity = direction
--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -3,10 +3,11 @@ extends CharacterBody2D
 @onready var player = get_node("/root/Main/player")
 @onready var candle = get_node("/root/Main/candle")
 
+func _ready():
+	pass
+
 func _physics_process(delta):
 		var direction = global_position.direction_to(candle.global_position)
 		velocity = direction * 60
 		move_and_slide()
-
-
 
