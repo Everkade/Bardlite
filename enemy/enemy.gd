bd44c3712e9c5c4ef024283babda6f9a09770f08
Kyle Darian <kyleenarson@gmail.com>
2024-02-01 20:37:12 -0500

    State Changes

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -1,13 +1,11 @@
 extends CharacterBody2D
+class_name Enemy
 
 @onready var player = get_node("/root/Main/player")
-@onready var candle = get_node("/root/Main/candle")
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-		var direction = global_position.direction_to(candle.global_position)
-		velocity = direction * 60
-		move_and_slide()
+	move_and_slide()
 
