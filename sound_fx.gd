cd5cdc5cf8550f1c42d2859eb09ef937156262ca
XeranicG <xelanicgamer@gmail.com>
2024-02-01 23:59:56 -0500

    More sfx

--- sound_fx.gd
+++ sound_fx.gd
@@ -3,9 +3,44 @@ extends Node
 func button_click():
 	
 	$"ButtonClick".play()
-	
 
 
 func footstep():
 	
 	$"Footstep".play()
+
+
+func croaker():
+	
+	$"Croaker".play()
+
+
+func crawler_growl():
+	
+	$"CrawlerGrowl".play()
+
+
+func death_bunny():
+	
+	$"DeathBunny".play()
+
+
+func attack_one():
+	
+	$"AttackSound".play()
+
+
+func attack_two():
+	
+	$"AttackSound2".play()
+
+
+func player_hurt():
+	
+	$"PlayerHurt".play()
+
+func death_jingle():
+	
+	$"DeathJingle".play()
+
+
